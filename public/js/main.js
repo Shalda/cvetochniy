09040bc0263cd82203ab22305c59da8cc66aa7f1
shalda <zeppelin.ua@gmail.com>
2019-04-05 18:31:02 +0300

    main video, remove requirejs

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,41 @@
-requirejs.config({
-    baseUrl: './public/js'
-});
+"use strict";
+document.getElementById(['nav-icon1']).addEventListener("click", toggleClass);
 
-define(["jquery", "domReady"], function ($) {
-    "use strict";
-    document.getElementById(['nav-icon1']).addEventListener("click", toggleClass);
-    function toggleClass() {
-        this.classList.toggle('open');
-        document.getElementById(['nav-menu']).classList.toggle('open')
-    }
-    document.querySelector('.nav_menu_left').addEventListener("mouseover", ClassText);
+function toggleClass() {
+    this.classList.toggle('open');
+    document.getElementById(['nav-menu']).classList.toggle('open')
+}
 
-    function ClassText(e) {
-        const classMenu = e.target.classList[0];
-        if (classMenu) {
-            switch (classMenu) {
-                case 'about':
-                    classTextToogle('about');
-                    break;
-                case 'cooperation':
-                    classTextToogle('cooperation');
-                    break;
-                case 'delivery':
-                    classTextToogle('delivery');
-                    break;
-                default:
-                    return;
-            }
+document.querySelector('.nav_menu_left').addEventListener("mouseover", ClassText);
+
+function ClassText(e) {
+    const classMenu = e.target.className;
+    if (classMenu) {
+        switch (classMenu) {
+            case 'about':
+                setTimeout(classTextToogle, 200, 'about');
+                break;
+            case 'shop':
+                setTimeout(classTextToogle, 200, 'shop');
+                break;
+            case 'cooperation':
+                setTimeout(classTextToogle, 200, 'cooperation');
+                break;
+            case 'delivery':
+                setTimeout(classTextToogle, 200, 'delivery');
+                break;
+            default:
+                return;
         }
     }
-    function classTextToogle(e) {
-        let olreadyOpen =  document.querySelector('.menu_right_text.open');
-        if(olreadyOpen){
-            olreadyOpen.classList.remove('open');
-        }
-        setTimeout(function (){document.getElementById(e).classList.add('open')} , 200)
+}
 
+function classTextToogle(e) {
+    let olreadyOpen = document.querySelector('.menu_right_text.open');
+    if (olreadyOpen) {
+        olreadyOpen.classList.remove('open');
     }
-});
+    document.getElementById(e).classList.add('open');
+
+}
+document.getElementById('video').style.height = (document.documentElement.clientHeight - 170) + 'px';
