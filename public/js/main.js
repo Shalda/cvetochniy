99ce1fad4fd1b77b448eaba1129e3a6c69e93116
shalda <zeppelin.ua@gmail.com>
2019-04-26 13:00:05 +0300

    footer popup

--- public/js/main.js
+++ public/js/main.js
@@ -82,7 +82,66 @@ $(document).ready(function () {
             e.preventDefault();
             $(this).find('ul').addClass('open_submenu').slideToggle();
         });
+
+
+
 });
+
+
+
+// PopUp Form and thank you popup after sending message
+var $popOverlay = $(".consultation_popup-overlay");
+var $popWindow = $(".consultation_popWindow");
+var $subscribeWindow = $(".consultation_subscribe_window");
+var $popThankYouWindow = $(".thank_you_window");
+var $popClose = $(".consultation_close-btn");
+var $popUp = $('#consultationPopup');
+$(function() {
+    // Close Pop-Up after clicking on the button "Close"
+    $popClose.on("click", function() {
+        $popOverlay.fadeOut();
+        $popWindow.fadeOut();
+    });
+
+    // Close Pop-Up after clicking on the Overlay
+    $(document).on("click", function(event) {
+        if ($(event.target).closest($popWindow).length) return;
+        if ($(event.target).closest($popUp).length) return;
+        $popOverlay.fadeOut();
+        $popWindow.fadeOut();
+        event.stopPropagation();
+    });
+
+    // Form Subscribe
+    $(".consultation_subscribe-form").submit(function() {
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: th.serialize()
+        }).done(function() {
+            // после успешной отправки скрываем форму подписки и выводим окно с благодарностью за заполнение формы
+            $subscribeWindow.fadeOut();
+            $popThankYouWindow.fadeIn();
+            // используем куки на 30 дней, если человек заполнил форму
+            // для куки обязательно должен быть подключен jquery.cookie.min.js
+            //$.cookie('hideTheModal', 'true', { expires: 30 });
+            // очищаем форму
+            setTimeout(function() {
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+});
+
+// используйте этот код, если нужно появление формы без куки
+
+$popUp.on('click', function(){
+    $popOverlay.fadeIn();
+    $subscribeWindow.fadeIn();
+});
+    
 
 //googlemaps
  function initMap() {
