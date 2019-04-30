e1303274f5d0fb92505a6a96a03bfb68ffd9cf16
shalda <zeppelin.ua@gmail.com>
2019-04-30 12:19:25 +0300

    slow fix 3

--- public/js/main.js
+++ public/js/main.js
@@ -10,38 +10,38 @@
     }
 
 //text on dropdown menu
-    document.querySelector('.nav_menu_left').addEventListener("click", ClassText);
-
-    function ClassText(e) {
-        const classMenu = e.target.className;
-        if (classMenu) {
-            switch (classMenu) {
-                case 'about':
-                    setTimeout(classTextToogle, 200, 'about');
-                    break;
-                case 'shop':
-                    setTimeout(classTextToogle, 200, 'shop');
-                    break;
-                case 'cooperation':
-                    setTimeout(classTextToogle, 200, 'cooperation');
-                    break;
-                case 'delivery':
-                    setTimeout(classTextToogle, 200, 'delivery');
-                    break;
-                default:
-                    return;
-            }
-        }
-    }
-
-    function classTextToogle(e) {
-        let olreadyOpen = document.querySelector('.menu_right_text.open');
-        if (olreadyOpen) {
-            olreadyOpen.classList.remove('open');
-        }
-        document.getElementById(e).classList.add('open');
-
-    }
+//     document.querySelector('.nav_menu_left').addEventListener("click", ClassText);
+//
+//     function ClassText(e) {
+//         const classMenu = e.target.className;
+//         if (classMenu) {
+//             switch (classMenu) {
+//                 case 'about':
+//                     setTimeout(classTextToogle, 200, 'about');
+//                     break;
+//                 case 'shop':
+//                     setTimeout(classTextToogle, 200, 'shop');
+//                     break;
+//                 case 'cooperation':
+//                     setTimeout(classTextToogle, 200, 'cooperation');
+//                     break;
+//                 case 'delivery':
+//                     setTimeout(classTextToogle, 200, 'delivery');
+//                     break;
+//                 default:
+//                     return;
+//             }
+//         }
+//     }
+//
+//     function classTextToogle(e) {
+//         let olreadyOpen = document.querySelector('.menu_right_text.open');
+//         if (olreadyOpen) {
+//             olreadyOpen.classList.remove('open');
+//         }
+//         document.getElementById(e).classList.add('open');
+//
+//     }
 
     //dropdownmenu mobile
     //
